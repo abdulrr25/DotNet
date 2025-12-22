a641b75a72115c6a21050b27f90d31f4c5b26a17
abdulrr25 <abdulr6503@gmail.com>
2025-12-22 19:35:20 +0530

    22 DEC

---
+++ Solution1/07Day_PartialClass/File1/CMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Day_PartialClass
+{
+    internal partial class CMath
+    {
+
+        public int Add(int x,  int y)
+        {
+            return (x+y);
+        }
+    }
+}
---
+++ Solution1/07Day_PartialClass/File2/CMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Day_PartialClass
+{
+    internal partial class CMath
+    {
+
+        public int Mul(int x, int y)
+        {
+
+            return (x*y);
+
+        }
+    }
+}

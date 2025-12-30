d426152b42ff8a4da84aedb5d4c524c15701662d
abdulrr25 <abdulr6503@gmail.com>
2025-12-30 19:04:14 +0530

    30 Dec 25

---
+++ Solution1/11Day_GarbageCollector/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _11Day_GarbageCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.sayhi();
+            obj.Dispose();
+
+
+            using (MyClass obj2 = new MyClass())
+            {
+                obj2.sayhi();
+            }
+
+            GC.Collect(2);
+            Console.WriteLine(GC.MaxGeneration);
+
+            MyClass obj3 = new MyClass();
+            Console.WriteLine(GC.GetGeneration(obj3));
+
+            GC.WaitForPendingFinalizers();
+
+
+
+        }
+    }
+
+    public class MyClass : IDisposable
+    {
+        public void sayhi()
+        {
+            Console.WriteLine("Hello from Garbage Collector");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose method gets called...");
+            GC.SuppressFinalize(this);
+        }
+
+        
+        
+        //~MyClass()
+        //{
+        //    Console.WriteLine("Destructor Called");
+        //}
+    
+    }
+
+}
---
+++ Solution1/11Day_Threading/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _11Day_Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ByDefault threads are nameless
+             Thread t = Thread.CurrentThread;
+            t.Name = "Main Thread";
+            //Console.WriteLine(t.ManagedThreadId);
+            //Console.WriteLine(t.Name);
+            #endregion
+
+            //method1();
+            //method2();
+            //method3();
+
+            Thread t1 = new Thread(method1) { Name = "th 01" };
+            Thread t2 = new Thread(method2) { Name = "th 02" };
+            Thread t3 = new Thread(method3) { Name = "th 03" };
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            ThreadStart ts = new ThreadStart(() =>
+            {
+                Console.WriteLine("Method Call...");
+            });
+            Thread t4 = new Thread(ts);
+            t4.Start();
+
+
+        }
+
+        static void method1()
+        {
+            Console.WriteLine("Method1 Started using {0}", Thread.CurrentThread.Name);
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("Method1 - Count: {0}", i);
+                //Thread.Sleep(5000);
+            
+            }
+            Console.WriteLine("Method1 Ended using {0}",Thread.CurrentThread.Name);
+
+
+        }
+
+        static void method2()
+        {
+
+            Console.WriteLine("Method2 Started using {0}", Thread.CurrentThread.Name);
+            //Console.WriteLine("Method2 - Count: {0}", Thread.CurrentThread.Name);
+            for (int i = 0;i < 5; i++)
+            {
+                Console.WriteLine("Method2 - Count: {0}", i);
+                if (i == 2)
+                {
+                    Console.WriteLine("Execution code started");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Execution code Completed");
+
+                }
+            }
+            //Console.WriteLine("Method2 - Count: {0}", Thread.CurrentThread.Name);
+            Console.WriteLine("Method1 Ended using {0}", Thread.CurrentThread.Name);
+
+
+
+        }
+
+        static void method3()
+        {
+            Console.WriteLine("Method3 - Count: {0}", Thread.CurrentThread.Name);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Method3 - Count: {0}", i);
+            }
+            Console.WriteLine("Method3 - Count: {0}", Thread.CurrentThread.Name);
+
+
+        }
+    }
+}
